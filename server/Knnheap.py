c45dacdc724f3161a4e6f720c9f99184330c232e
paoloav26 <86452704+paoloav26@users.noreply.github.com>
2022-07-06 00:33:31 -0500

    Update Knnheap.py

--- server/Knnheap.py
+++ server/Knnheap.py
@@ -7,26 +7,25 @@ import heapq
 pickle_in = open("dict.pickle","rb")
 data_vec=pickle.load(pickle_in)
 
-print(data_vec)
 route="./IMG_4300mod.jpg"
 encoding = face_recognition.face_encodings(face_recognition.load_image_file(route))[0]
-print(encoding)
 
 def KnnHeap(Query,k,data):
     result=[]
-    for i in data_vec:
-        for j in data_vec[i]:
+    for i in data:
+        for j in data[i]:
             dist=face_recognition.face_distance([Query],j)[0]
             heapq.heappush(result,(dist,i))
     return list(heapq.heappop(result) for i in range(k))
 
-def KnnRange(Query,range):
+def RangeSearch(Query,range,data):
     result=[]
-    for i in data_vec:
-        for j in data_vec[i]:
+    for i in data:
+        for j in data[i]:
             dist=face_recognition.face_distance([Query],j)[0]
             if(dist<range): result.append(i)
     return result
 
 
 print(KnnHeap(encoding,10,data_vec))
+#print(RangeSearch(encoding,2,data_vec))
