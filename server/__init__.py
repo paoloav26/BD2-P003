0075dfaa537578c04cc478d581d5ed548923b2d2
paoloav26 <86452704+paoloav26@users.noreply.github.com>
2022-07-10 16:04:38 -0500

    execution time and k number added

--- server/__init__.py
+++ server/__init__.py
@@ -11,6 +11,7 @@ from KnnHighd import KnnHighD
 from KNNRtree import KnnRtreee
 import face_recognition
 import pickle
+import time
 
 
 def create_app(test_config=None):
@@ -23,6 +24,7 @@ def create_app(test_config=None):
         
         KNNheap=[]
         KNNHighd=[]
+        KNNRtree=[]
         error_msg=""
 
         if request.method == "POST":
@@ -30,6 +32,7 @@ def create_app(test_config=None):
             if request.files:
                 error_msg=""
                 image = request.files["image"]
+                k = int(request.form.get("knumber"))
 
                 image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
 
@@ -42,15 +45,32 @@ def create_app(test_config=None):
                     pickle_in.close()
 
                     image_photo="./static/"+image.filename
-                    KNNheap=KnnHeap(encoding,5,data)
-                    KNNHighd=KnnHighD(encoding,5,data)
-                    KNNR_1=KnnRtreee(encoding,5,data)
-                    print(KNNR_1)
+                    
+                    start_time = time.time()
+                    KNNheap=KnnHeap(encoding,k,data)
+                    KNNheap_time = round(time.time() - start_time,8)
+
+                    start_time = time.time()
+                    KNNHighd=KnnHighD(encoding,k,data)
+                    KNNHighd_time = round(time.time() - start_time,8)
+
+                    start_time = time.time()
+                    KNNRtree=KnnRtreee(encoding,k,data)
+                    KnnRtreee_time = round(time.time() - start_time,8)
+
                 except Exception as e:
                     print(e)
                     error_msg="ERROR: Esa imagen no puede ser procesada"
 
-            return render_template("index.html",error_msg=error_msg,input_photo=image_photo,Knnheap=KNNheap,KnnHighD=KNNHighd,KnnR=KNNR_1)
+            return render_template("index.html",
+            error_msg=error_msg,
+            input_photo=image_photo,
+            Knnheap=KNNheap,
+            KNNheap_time=KNNheap_time,
+            KnnHighD=KNNHighd,
+            KNNHighd_time = KNNHighd_time,
+            KnnR=KNNRtree,
+            KnnRtreee_time=KnnRtreee_time)
 
             
         return render_template("index.html")
